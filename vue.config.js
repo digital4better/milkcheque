7ee7ed0538b4233dfd3f030b7e5b6390c7d698a9
Lola Gauchet <lola@MacBook-Pro-de-Lola.local>
2021-03-22 09:33:06 +0100

    title modified

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "MilkCheque";
+      return args;
+    });
+  },
   publicPath: "/" + (process.env.PUBLIC_PATH || "")
 };
